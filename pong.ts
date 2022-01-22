2cd16a5d9d40b21404fd9e7b242903aba91d7fab
Usama Mian <38145675+usamaqasimmian@users.noreply.github.com>
2022-01-23 02:35:34 +0500

    Update pong.ts

--- pong.ts
+++ pong.ts
@@ -23,15 +23,6 @@ function pongGame(startonce :Subscription) :void {
     player2current: number = 0,
     pongSpeed :HTMLSelectElement = document.getElementById("speed") as HTMLSelectElement,
     ponguserspeed = Number(pongSpeed.options[pongSpeed.selectedIndex].value);
-  // Inside this function you will use the classes and functions 
-  // from rx.js
-  // to add visuals to the svg element in pong.html, animate them, and make them interactive.
-  // Study and complete the tasks in observable exampels first to get ideas.
-  // Course Notes showing Asteroids in FRP: https://tgdwyer.github.io/asteroids/ 
-  // You will be marked on your functional programming style
-  // as well as the functionality that you implement.
-  // Document your code!  
-
 
   type State = Readonly<{
     x: number;
